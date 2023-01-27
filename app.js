5f62a03c66f96a7f95cfc6d3e5a5afa9d20e4446
Johnny Bieniek <jjbieniek@gmail.com>
2023-01-26 19:39:49 -0500

    initial commit with MongoDB successfully connected to the project

---
+++ app.js
@@ -0,0 +1,65 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: {
+    type: String,
+    required: true,
+  },
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const task1 = new Item({ name: "Homework" });
+const task2 = new Item({ name: "Read a book" });
+const task3 = new Item({ name: "Talk to family" });
+
+const defaultItems = [task1, task2, task3];
+
+Item.insertMany(defaultItems, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("success");
+  }
+});
+
+app.get("/", function (req, res) {
+  res.render("list", { listTitle: "Today", newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
